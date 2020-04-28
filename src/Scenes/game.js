32539eead281567dbce3f6faa39aed397e1635e1
HarishgunaS <satgu7@gmail.com>
2020-04-27 23:02:06 -0400

    split player into separate object file

--- src/Scenes/game.js
+++ src/Scenes/game.js
@@ -14,56 +14,8 @@ class HUD
         this.coordinates.setText('Coordinates: ' + Math.round(this.player.getSprite().x) + ", " + Math.round(this.player.getSprite().y));
     }
 }
-class Player 
-{
-    constructor(scene, data, sprite_key)
-    {
-        this.speed = data.speed;
-        this.health = data.health;
-        this.sprite = scene.physics.add.sprite(data.x, data.y, sprite_key);
-        this.sprite.setCollideWorldBounds(true);
-        scene.cameras.main.startFollow(this.sprite);
-    }
-    update(cursors)
-    {
-        if (cursors.down.isDown)
-        {
-            this.sprite.setVelocityY(this.speed);
-        }
-        else if (cursors.up.isDown)
-        {
-            this.sprite.setVelocityY(-this.speed);
-        }
-        else
-        {
-            this.sprite.setVelocityY(0);
-        }
-
-        if (cursors.right.isDown)
-        {
-            this.sprite.setVelocityX(this.speed);
-        }
-        else if (cursors.left.isDown)
-        {
-            this.sprite.setVelocityX(-this.speed);
-        }
-        else
-        {
-            this.sprite.setVelocityX(0);
-        }
-    }
-    getSprite()
-    {
-        return this.sprite;
-    }
-    getHealth()
-    {
-        return this.health;
-    }
-}
 class Game extends Phaser.Scene
 {
-    
     constructor()
     {
         super("playGame");
