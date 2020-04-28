99c952fd217566388047673dcf87f33bcbf99572
HarishgunaS <satgu7@gmail.com>
2020-04-28 18:38:27 -0400

    Fixed movement so diagonal is same speed

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -3,6 +3,7 @@ class Player
     constructor(scene, data, sprite_key)
     {
         this.speed = data.speed;
+        this.current_speed = this.speed;
         this.health = data.health;
         this.sprite = scene.physics.add.sprite(data.x, data.y, sprite_key);
         this.sprite.setCollideWorldBounds(true);
@@ -10,13 +11,21 @@ class Player
     }
     update(cursors)
     {
+        if ((cursors.down.isDown || cursors.up.isDown) && (cursors.left.isDown || cursors.right.isDown))
+        {
+            this.current_speed = this.speed/(Math.sqrt(2));
+        }
+        else
+        {
+            this.current_speed = this.speed;
+        }
         if (cursors.down.isDown)
         {
-            this.sprite.setVelocityY(this.speed);
+            this.sprite.setVelocityY(this.current_speed);
         }
         else if (cursors.up.isDown)
         {
-            this.sprite.setVelocityY(-this.speed);
+            this.sprite.setVelocityY(-this.current_speed);
         }
         else
         {
@@ -25,11 +34,11 @@ class Player
 
         if (cursors.right.isDown)
         {
-            this.sprite.setVelocityX(this.speed);
+            this.sprite.setVelocityX(this.current_speed);
         }
         else if (cursors.left.isDown)
         {
-            this.sprite.setVelocityX(-this.speed);
+            this.sprite.setVelocityX(-this.current_speed);
         }
         else
         {
