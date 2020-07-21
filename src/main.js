b5c864a63dddd45b9b17510c2c0381c1810990eb
Harishguna Satgunarajah <satgu7@gmail.com>
2020-07-21 00:10:28 -0400

    fixing sizing

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-
 var config = {
     type: Phaser.AUTO,
     scale: {
-        mode: Phaser.Scale.RESIZE,
-        parent: null,
-        width: '80%',
-        height: '80%'
+        parent: 'game',
+        mode: Phaser.Scale.FIT,
+        width: 800,
+        height: 600,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
+    autoRound: false,
     physics: {
         default: 'arcade',
         arcade: {
