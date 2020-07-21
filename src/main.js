3cbc7e743c10bdb895c208f97e9b0b6b2adbaa2f
Harishguna Satgunarajah <satgu7@gmail.com>
2020-07-20 23:13:13 -0400

    trying autosized canvas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
     physics: {
         default: 'arcade',
         arcade: {
